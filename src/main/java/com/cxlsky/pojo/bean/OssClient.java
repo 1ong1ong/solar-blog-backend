52b41ee2b0667fa6c3eecfdfc297acd27c255882
chenxl <chenxiaolong@china-goldcard.com>
2020-03-05 08:53:40 +0800

    上传图片

--- src/main/java/com/cxlsky/pojo/bean/OssClient.java
+++ src/main/java/com/cxlsky/pojo/bean/OssClient.java
@@ -3,16 +3,9 @@ package com.cxlsky.pojo.bean;
 import io.minio.MinioClient;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.io.Closeable;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
 
 @Data
 @Slf4j
@@ -32,29 +25,35 @@ public class OssClient {
     }
 
     public String upload(MultipartFile file, String bucketName, String fileName) {
-        if (file.isEmpty()) {
+        String fileUrl = "";
+        if (!file.isEmpty()) {
             log.error("文件为空");
+            return fileUrl;
         }
-        if (file.isEmpty()) {
-            System.out.println("文件为空空");
-        }
-        String originalFilename = file.getOriginalFilename();// 文件名
+        String originalFilename = file.getOriginalFilename(); // 文件名
         String userDIr = System.getProperty("user.dir") + File.separator + "temp" + File.separator;
         String newPath = userDIr + originalFilename;
         File dest = new File(newPath);
         if (!dest.getParentFile().exists()) {
-            boolean mkdirs = dest.getParentFile().mkdirs();
+            dest.getParentFile().mkdirs();
         }
         try {
             file.transferTo(dest);
             minioClient.putObject(bucketName, fileName, newPath);
-            return host + bucketName + fileName;
+            deleteTempFile(dest);
+            fileUrl = host + bucketName + fileName;
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return "";
+        return fileUrl;
 
     }
 
+    private void deleteTempFile(File file) {
+        boolean delete = file.delete();
+        if (!delete) {
+            file.deleteOnExit();
+        }
+    }
 }
